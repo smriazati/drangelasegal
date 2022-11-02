2580117decac13ef72fa09b867cab525d8c3cfd4
smriazati <sarahriazati@gmail.com>
2022-11-02 11:26:08 -0400

    add newsletter standalong page

---
+++ pages/newsletter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text-center page-container newsletter-page">
+      <FooterNewsletter />
+    </div>
+  </template>
+  <script>
+  export default {};
+  </script>
+  <style lang="scss">
+@media (max-width: $collapse-bp) {
+  .newsletter-page .newsletter-form {
+    padding: 0 24px;
+  }
+  .newsletter-page .newsletter-form h2 {
+    text-align: center;
+    max-width: 100%;
+  } 
+}
+  </style>
