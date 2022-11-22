a707a81350e27f15ccddd93b0966444ed1b79dd9
smriazati <sarahriazati@gmail.com>
2022-11-22 09:03:10 -0800

    update typeface sizes

--- components/Footer/Disclaimer.vue
+++ components/Footer/Disclaimer.vue
@@ -14,15 +14,19 @@
     </div>
     <nav class="footer-nav-secondary">
       <ul display="flex">
-        <li><nuxt-link to="/terms">Terms of Use</nuxt-link></li>
-        <li><nuxt-link to="/privacy">Privacy Policy</nuxt-link></li>
+        <li>
+          <nuxt-link to="/terms">Terms of Use</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/privacy">Privacy Policy</nuxt-link>
+        </li>
       </ul>
     </nav>
   </div>
 </template>
     
     
-  <script>
+<script>
 import { groq } from "@nuxtjs/sanity";
 const query = groq`*[_type == "footer"]{
     disclaimer
@@ -51,39 +55,49 @@ export default {
   @media (min-width: $collapse-bp) {
     margin-top: 60px;
   }
+
   @media (max-width: $collapse-bp) {
     margin-top: 32px;
   }
+
   .disclaimer {
     max-width: 1040px;
     margin: 0 auto;
+
     p {
       @include footerText;
+      font-size: 11px;
     }
   }
+
   .copyright {
     margin-top: 24px;
   }
+
   .copyright p,
   .copyright p a,
   .footer-nav-secondary li,
   .footer-nav-secondary li a {
     @include footerDetailText;
   }
+
   .copyright p,
   .footer-nav-secondary li {
     &:not(:last-child) {
       margin-right: 8px;
     }
   }
+
   .copyright,
   .footer-nav-secondary {
     display: flex;
     justify-content: center;
+
     @media (max-width: $collapse-bp) {
       justify-content: flex-start;
     }
   }
+
   .footer-nav-secondary li:not(:last-child):after {
     content: "|";
     margin-left: 8px;
