3e0a8ee8d3e1873ccddb54d4de2b22e0d0903a34
smriazati <sarahriazati@gmail.com>
2023-02-24 16:09:13 -0500

    offerings page updates

--- pages/offerings.vue
+++ pages/offerings.vue
@@ -1,69 +1,37 @@
 <template>
   <div class="offerings-page no-page-padding" ref="top">
     <div class="offerings-bg-imgs stacked-divs">
-      <div
-        v-if="data.offering1"
-        :class="activeOffering === 1 ? 'show' : 'hide'"
-        :lazy-background="
-          $urlFor(data.offering1.img.url).width(2000).auto('format')
-        "
-        class="bg-full-width bg-fixed"
-      ></div>
-      <div
-        v-if="data.offering2"
-        :class="activeOffering === 2 ? 'show' : 'hide'"
-        :lazy-background="
-          $urlFor(data.offering2.img.url).width(2000).auto('format')
-        "
-        class="bg-full-width bg-fixed"
-      ></div>
-      <div
-        v-if="data.offering3"
-        :class="activeOffering === 3 ? 'show' : 'hide'"
-        :lazy-background="
-          $urlFor(data.offering3.img.url).width(2000).auto('format')
-        "
-        class="bg-full-width bg-fixed"
-      ></div>
-      <div
-        v-if="data.offering4"
-        :class="activeOffering === 4 ? 'show' : 'hide'"
-        :lazy-background="
-          $urlFor(data.offering4.img.url).width(2000).auto('format')
-        "
-        class="bg-full-width bg-fixed"
-      ></div>
+      <div v-if="data.offering1" :class="activeOffering === 1 ? 'show' : 'hide'" :lazy-background="
+        $urlFor(data.offering1.img.url).width(2000).auto('format')
+      " class="bg-full-width bg-fixed"></div>
+      <div v-if="data.offering2" :class="activeOffering === 2 ? 'show' : 'hide'" :lazy-background="
+        $urlFor(data.offering2.img.url).width(2000).auto('format')
+      " class="bg-full-width bg-fixed"></div>
+      <div v-if="data.offering3" :class="activeOffering === 3 ? 'show' : 'hide'" :lazy-background="
+        $urlFor(data.offering3.img.url).width(2000).auto('format')
+      " class="bg-full-width bg-fixed"></div>
+      <div v-if="data.offering4" :class="activeOffering === 4 ? 'show' : 'hide'" :lazy-background="
+        $urlFor(data.offering4.img.url).width(2000).auto('format')
+      " class="bg-full-width bg-fixed"></div>
     </div>
 
     <div class="offerings-page-wrapper">
-      <button @click="setActiveOffering(1)"><span>Workshops</span></button>
-      <button @click="setActiveOffering(2)"><span>Individuals</span></button>
+      <button @click="setActiveOffering(1)"><span>Individuals</span></button>
+      <button @click="setActiveOffering(2)"><span>Workshops</span></button>
       <button @click="setActiveOffering(3)"><span>Corporate</span></button>
       <button @click="setActiveOffering(4)"><span>Events</span></button>
 
       <div class="offerings-wrapper stacked-divs">
-        <div
-          v-if="data.offering1"
-          :class="activeOffering === 1 ? 'show' : 'hide'"
-        >
+        <div v-if="data.offering1" :class="activeOffering === 1 ? 'show' : 'hide'">
           <OfferingItem :data="data.offering1" />
         </div>
-        <div
-          v-if="data.offering2"
-          :class="activeOffering === 2 ? 'show' : 'hide'"
-        >
+        <div v-if="data.offering2" :class="activeOffering === 2 ? 'show' : 'hide'">
           <OfferingItem :data="data.offering2" />
         </div>
-        <div
-          v-if="data.offering3"
-          :class="activeOffering === 3 ? 'show' : 'hide'"
-        >
+        <div v-if="data.offering3" :class="activeOffering === 3 ? 'show' : 'hide'">
           <OfferingItem :data="data.offering3" />
         </div>
-        <div
-          v-if="data.offering4"
-          :class="activeOffering === 4 ? 'show' : 'hide'"
-        >
+        <div v-if="data.offering4" :class="activeOffering === 4 ? 'show' : 'hide'">
           <OfferingItem :data="data.offering4" />
         </div>
       </div>
@@ -71,7 +39,7 @@
   </div>
 </template>
   
-  <script>
+<script>
 import { groq } from "@nuxtjs/sanity";
 const query = groq`
 *[_type in ["offerings"]][0]{
@@ -171,15 +139,17 @@ export default {
 };
 </script>
   
-  <style lang="scss">
+<style lang="scss">
 .offerings-page {
   position: relative;
+
   .offerings-bg-imgs {
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
+
     &:after {
       content: '';
       position: absolute;
@@ -191,49 +161,62 @@ export default {
       mix-blend-mode: multiply;
       opacity: 0.5;
     }
-    > div {
+
+    >div {
       transition: 2s ease opacity;
+
       &.show.lazyLoad.isLoaded {
         opacity: 1;
       }
+
       &.hide.lazyLoad.isLoaded {
         opacity: 0;
       }
     }
   }
 }
+
 .offerings-page-wrapper {
   padding: 65px 16px;
   display: grid;
   width: 100%;
+
   @media (min-width: $collapse-bp) {
     grid-template-columns: 1fr minmax(720px, 1fr) 1fr;
   }
+
   @media (max-width: $collapse-bp) {
     grid-template-columns: 0.1fr 1fr 0.1fr;
     grid-gap: 16px;
   }
+
   grid-template-rows: 1fr auto 1fr;
   overflow: hidden;
+
   button {
     position: relative;
   }
+
   button:nth-child(1) {
     grid-row: 2 / 3;
     grid-column: 1 / 2;
   }
+
   button:nth-child(2) {
     grid-row: 1 / 2;
     grid-column: 2 / 3;
   }
+
   button:nth-child(3) {
     grid-row: 2 / 3;
     grid-column: 3 / 4;
   }
+
   button:nth-child(4) {
     grid-row: 3 / 4;
     grid-column: 2 / 3;
   }
+
   .offerings-wrapper {
     grid-row: 2 / 3;
     grid-column: 2 / 3;
@@ -241,90 +224,114 @@ export default {
 
   button {
     display: flex;
+
     span {
       @include inputStyle;
       color: $white;
       color: $sunlight;
       text-shadow: 0px 0px 8px $sunlight;
+
       @media (max-width: $collapse-bp) {
         display: none;
       }
     }
+
     &:before {
       content: "";
       background: url("/chevron-up.svg") center center no-repeat;
       width: 20px;
       height: 20px;
     }
+
     $animationDistance: 1rem;
+
     &:nth-child(1) {
       align-items: center;
+
       @media (max-width: $collapse-bp) {
         justify-content: flex-end;
       }
+
       span {
         margin-left: 12px;
       }
+
       &:before {
         transform: rotate(-90deg) translateY(0);
         transition: 0.3s ease all;
       }
+
       &:hover:before {
         transform: rotate(-90deg) translateY(-($animationDistance));
       }
     }
+
     &:nth-child(2) {
       flex-direction: column;
       align-items: center;
+
       span {
         margin-top: 12px;
       }
+
       &:before {
         transform: rotate(0deg) translateY(0);
         transition: 0.3s ease all;
       }
+
       &:hover:before {
         transform: rotate(0deg) translateY(-($animationDistance));
       }
     }
+
     &:nth-child(3) {
       flex-direction: row-reverse;
       align-items: center;
+
       @media (max-width: $collapse-bp) {
         justify-content: flex-end;
       }
+
       span {
         margin-right: 12px;
       }
+
       &:before {
         transform: rotate(90deg) translateY(0);
         transition: 0.3s ease all;
       }
+
       &:hover:before {
         transform: rotate(90deg) translateY(-($animationDistance));
       }
     }
+
     &:nth-child(4) {
       flex-direction: column-reverse;
       align-items: center;
+
       span {
         margin-bottom: 12px;
       }
+
       &:before {
         transform: rotate(180deg) translateY(0);
         transition: 0.3s ease all;
       }
+
       &:hover:before {
         transform: rotate(180deg) translateY(-($animationDistance));
       }
     }
   }
 }
+
 .offerings-wrapper {
-  > div {
+  >div {
     display: flex;
     transition: 1s ease transform;
     justify-content: center;
+
     @media (min-width: $collapse-bp) {
       margin: 52px;
     }
@@ -334,30 +341,34 @@ export default {
       height: auto;
     }
   }
-  > * {
+
+  >* {
     &.hide {
       height: 0;
     }
   }
-  > *:nth-child(1) {
+
+  >*:nth-child(1) {
     &.hide {
       transform: translateX(-100vw) translateY(0);
     }
   }
-  > *:nth-child(2) {
+
+  >*:nth-child(2) {
     &.hide {
       transform: translateX(0) translateY(calc(-100vh - $headerHeight));
     }
   }
-  > *:nth-child(3) {
+
+  >*:nth-child(3) {
     &.hide {
       transform: translateX(100vw) translateY(0);
     }
   }
-  > *:nth-child(4) {
+
+  >*:nth-child(4) {
     &.hide {
       transform: translateX(0) translateY(calc(100vh + $headerHeight));
     }
   }
-}
-</style>
+}</style>
