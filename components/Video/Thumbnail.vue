e644b94f3a1190cea482b6a52041a3211264eba7
smriazati <sarahriazati@gmail.com>
2023-02-28 21:56:31 -0500

    add video description hovers

--- components/Video/Thumbnail.vue
+++ components/Video/Thumbnail.vue
@@ -8,6 +8,9 @@
         <img :src="`https://via.placeholder.com/${imgWidth}x${imgHeight}/C4C4C4/C4C4C4?Text=`"
           alt="gray placeholder image" />
       </figure>
+      <figcaption v-if="item.description">
+        <p>{{ item.description }}</p>
+      </figcaption>
     </div>
 
     <div class="text-wrapper text-center">
@@ -44,6 +47,11 @@ export default {
   // @include stackedDivs;
   place-items: center;
   margin-bottom: 12px;
+  overflow: hidden;
+
+  figure {
+    display: flex;
+  }
 
   figure,
   img {
@@ -61,6 +69,32 @@ export default {
     background-size: contain;
     background-image: url("/play.svg");
     background-repeat: no-repeat;
+  }
+
+  &:hover {
+    figcaption {
+      opacity: 1;
+    }
+  }
+
+  figcaption {
+    opacity: 0;
+    transition: .5s ease all;
+    background: rgb(172 139 85 / 90%);
+    color: $white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    position: absolute;
+    padding: 15px;
+    top: 0;
+    width: 100%;
+    height: 100%;
+
+    * {
+      @include inputStyle;
+    }
   }
 }
 </style>
