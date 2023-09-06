9da537b8f964e13112988c38e5f21e7078344386
smriazati <sarahriazati@gmail.com>
2023-09-05 20:51:04 -0400

    update schedule page content and add cms integration for schedule page content

---
+++ components/Serializer/Link.vue
@@ -0,0 +1,30 @@
+<template>
+    <span>
+        <span v-if="type === 'internal'">
+            <nuxt-link :to="destination ? destination : '/'">
+                <slot />
+            </nuxt-link>
+        </span>
+        <span v-if="type === 'email'">
+            <a :href="`mailto:${email}`" :target="blank ? '_blank' : ''">
+                <slot />
+            </a>
+        </span>
+        <span v-if="type === 'external'">
+            <a :href="href" :target="blank ? '_blank' : ''">
+                <slot />
+            </a>
+        </span>
+    </span>
+</template>
+  
+<script>
+export default {
+    props: ['type', 'destination', 'href', 'blank', 'email']
+}
+</script>
+<style scoped>
+a {
+    text-decoration: underline;
+}
+</style>
--- pages/schedule.vue
+++ pages/schedule.vue
@@ -1,20 +1,42 @@
 <template>
   <div class="page-container schedule-page">
     <h1 class="heading-style text-center mb-24">Schedule</h1>
-    <p>Thank you for your interest in scheduling a session!  I have limited availability but if you are interested in
-      connecting to have a session, please email me directly at: <a
-        href="mailto:hello@angelasegalphd.com">hello@angelasegalphd.com</a>.</p>
+    <div v-if="data">
+      <SanityContent :blocks="data?.text.rte" :serializers="serializers"></SanityContent>
+    </div>
+    <div v-else>
+      <p>Thank you for your interest in scheduling a session!  Please email me directly at <a
+          href="mailto:angela@angelasegalphd.com">angela@angelasegalphd.com</a>.</p>
+    </div>
   </div>
 </template>
     
 <script>
+import { groq } from "@nuxtjs/sanity";
+import SerializerLink from '../components/Serializer/Link.vue'
+
+const query = groq`
+     *[_type in ["schedule"]][0]
+   `;
+
 
 export default {
   head() {
     return {
       title: "Schedule",
     };
-  }
+  },
+  async asyncData({ $sanity }) {
+    const data = await $sanity.fetch(query).then((res) => res);
+    return { data };
+  },
+  data: () => ({
+    serializers: {
+      marks: {
+        externalLink: SerializerLink,
+      }
+    }
+  })
 };
 </script>
 
