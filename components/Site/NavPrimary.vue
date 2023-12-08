3869e93c8d368479a2b6456b1770143b9d3ba05f
Kevin Olson <kevin.s.olson@gmail.com>
2023-12-08 10:06:19 -0500

    Video Lib replaces Learn in Nav Bar

--- components/Site/NavPrimary.vue
+++ components/Site/NavPrimary.vue
@@ -2,7 +2,7 @@
   <nav class="nav-primary">
     <ul>
       <li><nuxt-link to="/offerings">Offerings</nuxt-link></li>
-      <li><nuxt-link to="/learn">Learn</nuxt-link></li>
+      <li><nuxt-link to="/video-library">Videos</nuxt-link></li>
       <li><nuxt-link to="/journal">Journal</nuxt-link></li>
     </ul>
   </nav>
